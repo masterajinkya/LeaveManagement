b69cc07e8cfdc7bfb226e172887587b6bbe19aee
MasterAjinkya <137719809+masterajinkya@users.noreply.github.com>
2023-08-12 13:18:26 +0530

    Update Dashbord.js

--- src/LaveApplication/pages/Dashbord.js
+++ src/LaveApplication/pages/Dashbord.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Dashbord() {
   return (
     <div>
-      <img classname="imgs" src='https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/03/Hogwarts-Castle.jpg'/>
+      <img classname="figure-img img-fluid" src='https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/03/Hogwarts-Castle.jpg'/>
     </div>
   )
 }
