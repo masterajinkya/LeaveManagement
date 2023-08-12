7fac3c292c61d371ade3ffa71908e742704d40a2
masterajinkya <ajinkyagiri62@gmail.com>
2023-08-12 16:27:38 +0530

    Changes

--- src/LaveApplication/pages/Dashbord.js
+++ src/LaveApplication/pages/Dashbord.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Dashbord() {
   return (
     <div>
-      <img className='img-fluid imgs'  src='https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/03/Hogwarts-Castle.jpg'/>
+      <img className='img-fluid imgs' alt='Img' src='https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/03/Hogwarts-Castle.jpg'/>
     </div>
   )
 }
